33d0a14ffb512a6f5089cec6db123182f8e2632e
Brandon Craig <brandon.craig@guildeducation.com>
2020-07-08 17:37:46 -0600

    Add vector class

---
+++ vector.py
@@ -0,0 +1,37 @@
+class Vector(object):
+    def __init__(self, coordinates):
+        try:
+            if not coordinates:
+                raise ValueError
+            self.coordinates = tuple(coordinates)
+            self.dimension = len(coordinates)
+
+        except ValueError:
+            raise ValueError('The coordinates must be nonempty')
+
+        except TypeError:
+            raise TypeError('The coordinates must be an iterable')
+
+
+    def __str__(self):
+        return 'Vector: {}'.format(self.coordinates)
+
+
+    def __eq__(self, v):
+        return self.coordinates == v.coordinates
+
+    def __add__(self, v):
+        if self.dimension != v.dimension:
+            raise ValueError('Vectors must have same dimension')
+        new_coordinates = [x + y for x,y in zip(self.coordinates, v.coordinates)]
+        return Vector(new_coordinates)
+
+    def __sub__(self, v):
+        if self.dimension != v.dimension:
+            raise ValueError('Vectors must have same dimension')
+        new_coordinates = [x - y for x,y in zip(self.coordinates, v.coordinates)]
+        return Vector(new_coordinates)
+
+    def scale(self, s):
+        new_coordinates = [x * s for x in self.coordinates]
+        return Vector(new_coordinates)
